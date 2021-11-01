e79bdc00b3d4d91ceff7acbd3d9b1386c17abf96
bee-mcc <tuj03791@temple.edu>
2021-11-01 14:00:53 -0400

    added hello world

---
+++ src/main.py
@@ -0,0 +1,11 @@
+def main():
+    ourNames = ["Bri"]
+    decoration = "~~"
+    i = 1
+    for name in ourNames:
+        print(decoration*i + "Hello world, I'm " + name)
+        i += 1
+
+if __name__ == '__main__':
+    main()
+    
