1a0835fa24072a31cf0c98b9a129bbfadf7c98d6
bee-mcc <tuj03791@temple.edu>
2021-11-08 12:16:26 -0500

    working basic main

--- src/ScannerWorker.py
+++ src/ScannerWorker.py
@@ -3,9 +3,16 @@ try:
 except ImportError:
     import Image
 
-import pytesseract # only accepts rgb values so convert vals
+import pytesseract
 
 class ScannerWorker:
-       def test(self, path):
-              print(pytesseract.image_to_boxes(Image.open(path)))
+    def __init__(self, path) -> None:
+        self.path_to_image = path
+
+    def basicOCR(self):
+        return pytesseract.image_to_string(Image.open(self.path_to_image))
+
+    #TODO - get this to open a new image with boundary drawn onto the image
+    def genBoundaryBoxes(self):
+        return pytesseract.image_to_boxes(Image.open(self.path_to_image))
 
--- src/main.py
+++ src/main.py
@@ -5,19 +5,30 @@ import sys
 
 def main():
 
-    #initialize worker class
-    workerClass = ScannerWorker.ScannerWorker()
 
     #parse number of user command line arguments
     numArguments = len(sys.argv)
     #determine if they have their own file, or if they want to run our test functionality
     if numArguments == 1:
-        workerClass.test('../data/txt1.png')
+        workerClass = ScannerWorker.ScannerWorker('../data/txt1.png')
     elif numArguments == 2:
-        workerClass.test(sys.argv[1])
+        workerClass = ScannerWorker.ScannerWorker(sys.argv[1])
     else:
         print("please invoke the program with 0 or 1 arguments")
         
+    #MAIN logic
+    while(1):
+        print("Hello user. Thank you for using (beta) openHighlightCopyPaste. Not all functionality is avalible. Please choose your options below: ")
+        print("1) Basic OCR\n2) Generate Boundary boxes")
+        
+        choice = input("Enter choice: ")
+        choice = int(choice)
+        if choice == 1:
+            print(workerClass.basicOCR())
+        elif choice == 2:
+            print(workerClass.genBoundaryBoxes())
+        else:
+            print("please choose one of the options above.")
 
 
 if __name__ == '__main__':
